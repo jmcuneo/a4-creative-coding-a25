ff48977037594e30af3364ecf41441ac4c32a9b9
o-wel <owenvhart@gmail.com>
2025-09-29 22:36:13 -0400

    finalizes game

--- public/main.js
+++ public/main.js
@@ -7,9 +7,12 @@ let cssScaling;
 const missileWidth = 4;
 const missileHeight = 4;
 
+// will vary based on user
+let frameRate = 60;
+
 // classes
 class Missile {
-    constructor(x, y, angle, speed, launchPoint) {
+    constructor(x, y, angle, speed, launchPoint, type="normal") {
         this.x = x;
         this.y = y;
         this.angle = angle;
@@ -18,18 +21,49 @@ class Missile {
         this.launchPoint = launchPoint
         this.detonatePoint = null;
         this.launched = false;
+        this.exploded = false;
+        this.explodeRadius = 0;
+        this.explodeOut = true;
+        this.type = type;
+        this.scored = false;
     }
 
     update() {
-        this.x += Math.cos(this.angle) * this.speed;
-        this.y += Math.sin(this.angle) * this.speed;
+        if(this.exploded) {
+            if(this.explodeOut === true) {
+                this.explodeRadius += 0.25;
+            } else if (this.explodeRadius > 0 && this.explodeOut === false) {
+                this.explodeRadius -= 0.25;
+            }
 
-        if(this.path.length > 10){
-            this.path.shift()
-        } else if (Math.abs(this.x - this.path[this.path.length-1].x) > 4 ||
-                    Math.abs(this.y - this.path[this.path.length-1].y) > 4) {
-            this.path.push({x: this.x, y: this.y});
+            if(this.explodeRadius > 40) {
+                this.explodeOut = false
+            }
+
+            if(this.type === "enemy" && !this.scored) {
+                score += 25;
+                this.scored = true;
+            }
+
+        } else {
+            this.x += Math.cos(this.angle) * this.speed;
+            this.y += Math.sin(this.angle) * this.speed;
+
+            if(this.detonatePoint && Math.abs(this.x - this.detonatePoint.x) < 5 && Math.abs(this.y - this.detonatePoint.y) < 5) {
+                this.exploded = true;
+                if(this.type === "enemy") {
+                    gameOver = true
+                }
+            }
+
+            if(this.path.length > 10){
+                this.path.shift()
+            } else if (Math.abs(this.x - this.path[this.path.length-1].x) > 4 ||
+                Math.abs(this.y - this.path[this.path.length-1].y) > 4) {
+                this.path.push({x: this.x, y: this.y});
+            }
         }
+
     }
 
     launch(angle, speed, detonatePoint) {
@@ -43,19 +77,30 @@ class Missile {
     }
 
     draw(ctx) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.fillStyle = "blue";
-        ctx.fillRect(-missileWidth/2, -missileHeight/2, missileWidth, missileHeight);
-        ctx.restore();
-
-        ctx.fillStyle = "red";
-        this.path.forEach((p) => {
+        if(this.exploded){
             ctx.save();
-            ctx.translate(p.x, p.y);
-            ctx.fillRect(-1, 0, 2, 2);
+            ctx.beginPath();
+            ctx.fillStyle = "white";
+            ctx.arc(this.x, this.y, this.explodeRadius, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
             ctx.restore();
-        })
+        } else {
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.fillStyle = "blue";
+            ctx.fillRect(-missileWidth/2, -missileHeight/2, missileWidth, missileHeight);
+            ctx.restore();
+
+            ctx.fillStyle = "red";
+            this.path.forEach((p) => {
+                ctx.save();
+                ctx.translate(p.x, p.y);
+                ctx.fillRect(-1, 0, 2, 2);
+                ctx.restore();
+            })
+        }
+
     }
 }
 
@@ -64,18 +109,34 @@ let enemyMissles = [];
 let hills = [];
 let h1, h2, h3;
 let hillWidth, hillRadius, mid, leftRegion, rightRegion;
-
-const m = new Missile(100, 100, 45, 0.1)
+let level = 1;
+let time = 0;
+let score = 0;
+let gameOver = false;
+let gameRunning = false;
 
 // initialize
 window.onload = () => {
-    init();
+    const start = document.getElementById("start");
+    start.addEventListener("click", startGame)
+}
+
+function startGame() {
+    if (!gameRunning) {
+        level = 1;
+        score = 0;
+        init()
+        gameRunning = true;
+    }
 }
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     bounds = canvas.getBoundingClientRect();
+    myMissiles = [];
+    enemyMissles = [];
+    gameOver = false;
 
     // scaling to make mouse coords work
     cssScaling = canvas.width / bounds.width;
@@ -97,6 +158,7 @@ function init() {
 
     drawLand()
 
+    // setting up my missiles
     for (let h = 0; h < 3; h++) {
         let currentHill = hills[h];
 
@@ -129,6 +191,16 @@ function init() {
 
         myMissiles.push(missiles);
     }
+
+    // setting up enemy missiles
+    const enemyCount = Math.trunc(level * 2) + 3;
+
+    for(let i = 0; i < enemyCount; i++) {
+        const startX = Math.random() * (canvas.width-100);
+        const m = new Missile(startX, -50, 0, 0, {x: startX, y: -50}, "enemy");
+        enemyMissles.push(m);
+    }
+
 
     ctx.stroke();
     requestAnimationFrame(redraw)
@@ -182,14 +254,85 @@ function redraw() {
 
     myMissiles.forEach(missile => {
         missile.forEach(m => {
+            if(!m.exploded) {
+                checkMissileCollisions(m);
+            }
             m.update();
             m.draw(ctx);
         })
     })
 
+    let numToLaunch = 0;
+
+    // launches enemy missiles based on users frame rate
+    if(time % (frameRate * 5) === 0) {
+        numToLaunch = level;
+        time = 0;
+    }
+
+    enemyMissles.forEach(m => {
+        const targetX = Math.random() * (canvas.width-100);
+        const targetY = canvas.height - 50;
+
+        if(!m.launched && numToLaunch !== 0) {
+            let speed = 0.2 + level/100;
+            if(speed > 0.3) {
+                speed = 0.3;
+            }
+            m.launch(Math.atan2(targetY - m.launchPoint.y, targetX - m.launchPoint.x ), speed, {x: targetX, y: targetY});
+            numToLaunch--
+        }
+
+        if(!m.exploded) {
+            checkMissileCollisions(m);
+        }
+        m.update();
+        m.draw(ctx);
+    })
+
+    ctx.fillStyle = "white";
+    ctx.font = "15px Serif";
+    ctx.fillText(`${score}`, 20, 30);
+    ctx.fillText("Wave: " + level, canvas.width - 100, 30);
+
     ctx.restore();
 
-    requestAnimationFrame(redraw)
+    time++;
+
+    // check if wave is over
+    const allScored = enemyMissles.every(m => m.scored)
+    if(allScored && time % (frameRate * 5) === 0) {
+        time=0;
+        level++;
+        init()
+    } else if (gameOver === true) {
+        ctx.fillStyle = "red";
+        ctx.font = "50px Serif";
+        ctx.fillText("Game Over", canvas.width/2 - 100, canvas.height/2);
+        gameRunning = false;
+    } else {
+        requestAnimationFrame(redraw)
+    }
+}
+
+function checkMissileCollisions(checkMissile) {
+    const allMissiles = myMissiles.concat([enemyMissles]);
+
+    allMissiles.forEach(missileList => {
+        for (let m of missileList) {
+            if(m.exploded) {
+                const distX = Math.abs(checkMissile.x - m.x);
+                const distY = Math.abs(checkMissile.y - m.y);
+                const distance = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
+
+                if(distance < m.explodeRadius) {
+                    checkMissile.exploded = true;
+                    return;
+                }
+            }
+        }
+    })
+
 }
 
 addEventListener('mousedown', (e) => {
@@ -227,7 +370,7 @@ addEventListener('mousedown', (e) => {
 
     for(let m of missileSet) {
         if(!m.launched) {
-            m.launch(Math.atan2(mouseY - m.launchPoint.y, mouseX - m.launchPoint.x ), 0.4, {x: mouseX, y: mouseY});
+            m.launch(Math.atan2(mouseY - m.launchPoint.y, mouseX - m.launchPoint.x ), 0.5, {x: mouseX, y: mouseY});
             break;
         }
     }
