0db2289f9ad590decaeddf82a73a884d33d02d8c
Benedict A <sbenedict637@gmail.com>
2025-10-01 18:28:00 -0400

    Finished (At least that's what it seems)

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use( express.static( "public" ) );
+app.use( express.static( path.join(__dirname, "public" )));
 
 // Define a basic route
+app.use('/three', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/jsm', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 app.get('/', (req, res) => {
   // res.send('The basic Express server is running!');
   res.sendFile("public", "index.html")
