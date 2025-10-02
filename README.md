e1475cde255430201b5cb404ab6b303768a96117
gracm0 <grace.y.mahoney@gmail.com>
2025-10-02 01:20:41 -0400

    finished logic, almost done with readme

--- README.md
+++ README.md
@@ -2,11 +2,35 @@ Assignment 4 - Creative Coding: Interactive Multimedia Experiences
 ## Mahoney's Game of Life
 https://a4-gracemahoney.onrender.com
 
-Mahoney's Game of Life is a 2d cellular automata that resembles Conway's Game of Life. Each cell is alive (1) or dead (0). At each generation, rules apply based on 8 neighbors:
+Mahoney's Game of Life is an interactive 2d cellular automata that resembles Conway's Game of Life. Each cell is alive (1) or dead (0). At each generation, rules apply based on 8 neighbors:
 
+### Overcrowding Rule
+![Overcrowding Example](images/overcrowding.png)  
+*Cells with 8 neighbors die due to overcrowding.*
 
-Include a very brief summary of your project here. Images are encouraged when needed, along with concise, high-level text. Be sure to include the following:
+### Loneliness Rule
+![Loneliness Example](images/loneliness.png)  
+*Cells with fewer than 2 neighbors die due to loneliness.*
 
-- the goal of the application
-- challenges you faced in realizing the application
-- the instructions you present in the website should be clear enough to use the application, but if you feel any need to provide additional instructions please do so here.
+### Hysteria Rule
+![Hysteria Example](images/hysteria.png)  
+*Even number of neighbors increases chance of hysteria (i.e., death).*
+
+### Summoning Rule
+![Summoning Example](images/summoning.png)  
+*Dead cells with all 4 diagonal neighbors alive are born.*
+
+### Random Revival Rule
+![Random Revival Example](images/random_revival.png)  
+*Dead cells with exactly 2 neighbors have a 10% chance of coming alive.*
+
+### Conga Rule
+![Conga Example](images/conga.png)  
+*Cells with 7 neighbors die and rebirth is transferred to the dead neighboring cell.*
+
+## Goal of the Application
+The goal was to allow users to randomize the grid, paint alive cells, step through generations, and adjust simulation speed.
+
+## References
+https://www.w3schools.com/jsref/jsref_every.asp
+https://conwaylife.com/
--- main.js
+++ main.js
@@ -84,9 +84,48 @@ function nextGeneration() {
 
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            // TODO: replace with Conway or custom rules
-            if (r+1 < rows && c+1 < cols) newGrid[r][c] = grid[r+1][c+1];
-            else newGrid[r][c] = 0;
+            const aliveNeighbors = countNeighbors(r,c);
+            const cornersAlive = checkCorners(r,c);
+
+            if (grid[r][c] === 1) {
+                // Loneliness
+                if (aliveNeighbors < 2) newGrid[r][c] = 0;
+                // Overcrowding & Ostrasized
+                else if (aliveNeighbors % 2 === 0) {
+                    let chance;
+                    switch(aliveNeighbors) {
+                        case 2: chance = 0.05; break;
+                        case 4: chance = 0.2; break;
+                        case 6: chance = 0.7; break;
+                        case 8: chance = 1.0; break;
+                    }
+                    newGrid[r][c] = Math.random() < chance ? 0 : 1;
+                }
+                // Conga
+                else if (aliveNeighbors === 7) {
+                    newGrid[r][c] = 0;
+                    // Find the one dead neighbor to become alive
+                    for (let i = -1; i <= 1; i++) {
+                        for (let j = -1; j <= 1; j++) {
+                            if (i === 0 && j === 0) continue;
+                            const nr = r + i, nc = c + j;
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === 0) {
+                                newGrid[nr][nc] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                // Otherwise, stays alive
+                else newGrid[r][c] = 1;
+            } else {
+                // Summoning
+                if (cornersAlive) newGrid[r][c] = 1;
+                // Random Revival
+                else if (aliveNeighbors === 2 && Math.random() < 0.1) newGrid[r][c] = 1;
+                // Otherwise, stays dead
+                else newGrid[r][c] = 0;
+            }
         }
     }
     grid = newGrid;
@@ -94,6 +133,33 @@ function nextGeneration() {
     // Update generation count
     gen++;
     document.getElementById('generation').innerText = "Generation: " + gen;
+}
+
+// Simulation helper functions
+// Count all 8 neighbors
+function countNeighbors(r, c) {
+  let count = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      if (i === 0 && j === 0) continue;
+      const nr = r + i, nc = c + j;
+      if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) count += grid[nr][nc];
+    }
+  }
+  return count;
+}
+
+// Check 4 corners of a cell for Summoning rule
+function checkCorners(r, c) {
+  const corners = [
+    [r - 1, c - 1],
+    [r - 1, c + 1],
+    [r + 1, c - 1],
+    [r + 1, c + 1]
+  ];
+  return corners.every(([nr, nc]) =>
+    nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] === 1
+  );
 }
 
 // Controls
