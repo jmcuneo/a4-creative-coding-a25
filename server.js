70e9cf65e02a53541cabd788b5c14f4402e4c2ea
Ryan N <73202671+RyanNemeti@users.noreply.github.com>
2025-10-02 23:28:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use( express.static( 'public' ) )
 app.use( express.static( 'views'  ) ) 
@@ -12,4 +12,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`);
+
 })
