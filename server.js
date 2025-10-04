d149e2942179195685ec69693d8548c27a23f1aa
Cosmorb <njdriscoll@wpi.edu>
2025-10-03 23:04:10 -0400

    FIXED HTML TOOK XHESS/CHECKS STUFF OUT AS NOT USING USZEWING CANVAS

--- server.js
+++ server.js
@@ -128,3 +128,18 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// serve static files from "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// root route → serve index.html
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
